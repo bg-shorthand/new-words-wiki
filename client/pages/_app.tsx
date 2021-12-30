83092dcf71eee4f2bd587a64c3419e55fef9f6fc
bg-shorthand <hanhanhan333@gmail.com>
2021-12-30 15:20:03 +0900

    feat: 전체 레이아웃

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,13 +1,22 @@
-import '../styles/globals.css';
 import { RecoilRoot } from 'recoil';
 import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+import style from '../styles/layout.module.scss';
 import ModalDialogs from '@components/pages/ModalDialogs';
+import Header from '@organisms/header/Header';
+import Footer from '@organisms/footer/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <Component {...pageProps} />
-      <ModalDialogs />
+      <div className={style.container}>
+        <Header />
+        <main>
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+        <ModalDialogs />
+      </div>
     </RecoilRoot>
   );
 }
