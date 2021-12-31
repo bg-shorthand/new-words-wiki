759f6166b18033dd3d29fa0b7801c1f60ed771c1
bg-shorthand <hanhanhan333@gmail.com>
2021-12-31 21:48:23 +0900

    feat: SignupForm style

--- client/components/molecules/signupForm/SignupForm.tsx
+++ client/components/molecules/signupForm/SignupForm.tsx
@@ -1,10 +1,11 @@
 import Button from '@atoms/button/Button';
 import InputText from '@atoms/inputText/InputText';
 import Label from '@atoms/label/Label';
+import style from './SignupForm.module.scss';
 
 const SignupForm = () => {
   return (
-    <form>
+    <form className={style.container}>
       <Label htmlFor="signupId">ID</Label>
       <InputText id="signupId" />
       <Label htmlFor="signupPwd">Password</Label>
