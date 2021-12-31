0d66f1518c0be4318da2298e2304c49c568457f7
bg-shorthand <hanhanhan333@gmail.com>
2021-12-31 14:33:34 +0900

    feat: LoginForm markup

--- client/components/molecules/form/LoginForm.tsx
+++ client/components/molecules/form/LoginForm.tsx
@@ -1,13 +1,16 @@
 import Button from '@atoms/button/Button';
-import Input from '@atoms/input/Input';
+import Input from '@atoms/inputText/InputText';
+import Label from '@atoms/label/Label';
 
 const LoginForm = () => {
   return (
-    <>
-      <Input />
-      <Input />
+    <form>
+      <Label htmlFor="loginId">ID</Label>
+      <Input id="loginId" />
+      <Label htmlFor="loginPwd">Password</Label>
+      <Input id="loginPwd" />
       <Button>submit</Button>
-    </>
+    </form>
   );
 };
 
