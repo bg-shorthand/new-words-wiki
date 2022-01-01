d17de4da50c175e47cc9c0bb96db44bab748566d
bg-shorthand <hanhanhan333@gmail.com>
2022-01-01 11:26:45 +0900

    feat: signin state

--- client/components/molecules/signinForm/SigninForm.tsx
+++ client/components/molecules/signinForm/SigninForm.tsx
@@ -2,18 +2,22 @@ import Button from '@atoms/button/Button';
 import InputText from '@atoms/inputText/InputText';
 import Label from '@atoms/label/Label';
 import LabelInputBox from '@containers/labelInputContainer/LabelInputContainer';
+import { useState } from 'react';
 import style from './SigninForm.module.scss';
 
 const SigninForm = () => {
+  const [id, setId] = useState('');
+  const [pwd, setPwd] = useState('');
+
   return (
     <form className={style.container}>
       <LabelInputBox>
         <Label htmlFor="signinId">ID</Label>
-        <InputText id="signinId" />
+        <InputText id="signinId" value={id} onChange={(e) => setId(e.currentTarget.value)} />
       </LabelInputBox>
       <LabelInputBox>
         <Label htmlFor="signinPwd">Password</Label>
-        <InputText id="signinPwd" />
+        <InputText id="signinPwd" value={pwd} onChange={(e) => setPwd(e.currentTarget.value)} />
       </LabelInputBox>
       <Button>로그인</Button>
     </form>
