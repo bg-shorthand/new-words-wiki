d20dcf1db23dd7a649dfc722685126301b9f9f92
bg-shorthand <hanhanhan333@gmail.com>
2022-01-01 11:16:14 +0900

    modify: InputText value, onChange

--- client/components/atoms/inputText/InputText.tsx
+++ client/components/atoms/inputText/InputText.tsx
@@ -1,12 +1,28 @@
+import Button from '@atoms/button/Button';
 import { DefaultProps } from 'const/types';
+import { ChangeEventHandler } from 'react';
 
 interface InputProps extends DefaultProps {
   id: string;
+  value: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  password?: 'password';
   placeholder?: string;
 }
 
-const InputText = ({ id, placeholder }: InputProps) => {
-  return <input type="text" id={id} placeholder={placeholder} />;
+const InputText = ({ id, password, value, onChange, placeholder }: InputProps) => {
+  return (
+    <>
+      <input
+        type={password ? 'password' : 'text'}
+        value={value}
+        onChange={onChange}
+        id={id}
+        placeholder={placeholder}
+      />
+      {password ? <Button>비밀번호 보기</Button> : null}
+    </>
+  );
 };
 
 export default InputText;
