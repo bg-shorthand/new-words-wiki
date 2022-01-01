d071380ecb1ea768851ee7fefdd9f7f7f5e4a8b6
bg-shorthand <hanhanhan333@gmail.com>
2022-01-01 10:36:49 +0900

    feat: atom Alert

---
+++ client/components/atoms/alert/Alert.tsx
@@ -0,0 +1,7 @@
+import { DefaultProps } from 'const/types';
+
+const Alert = ({ children }: DefaultProps) => {
+  return <p>{children}</p>;
+};
+
+export default Alert;
--- client/components/molecules/signupForm/SignupForm.tsx
+++ client/components/molecules/signupForm/SignupForm.tsx
@@ -1,3 +1,4 @@
+import Alert from '@atoms/alert/Alert';
 import Button from '@atoms/button/Button';
 import InputText from '@atoms/inputText/InputText';
 import Label from '@atoms/label/Label';
@@ -10,14 +11,17 @@ const SignupForm = () => {
       <LabelInputBox locateLabel="top">
         <Label htmlFor="signupId">ID</Label>
         <InputText id="signupId" />
+        <Alert>email을 확인해주세요.</Alert>
       </LabelInputBox>
       <LabelInputBox>
         <Label htmlFor="signupPwd">Password</Label>
         <InputText id="signupPwd" />
+        <Alert>비밀번호를 확인해주세요.</Alert>
       </LabelInputBox>
       <LabelInputBox>
         <Label htmlFor="checkSignupPwd">Check Password</Label>
         <InputText id="checkSignupPwd" />
+        <Alert>비밀번호가 다릅니다.</Alert>
       </LabelInputBox>
       <Button>회원 가입</Button>
     </form>
