da7c70a834108c6c1886fbf0fa0f3bdad0d1ee2e
bg-shorthand <hanhanhan333@gmail.com>
2022-01-01 20:12:38 +0900

    feat: util style a11y-hidden

--- client/components/atoms/label/Label.tsx
+++ client/components/atoms/label/Label.tsx
@@ -1,11 +1,17 @@
 import { DefaultProps } from 'const/types';
+import utilStyle from '@styles/util.module.scss';
 
 interface LabelProps extends DefaultProps {
   htmlFor: string;
+  hidden?: boolean;
 }
 
-const Label = ({ htmlFor, children }: LabelProps) => {
-  return <label htmlFor={htmlFor}>{children}</label>;
+const Label = ({ htmlFor, children, hidden }: LabelProps) => {
+  return (
+    <label htmlFor={htmlFor} className={hidden ? utilStyle['a11y-hidden'] : ''}>
+      {children}
+    </label>
+  );
 };
 
 export default Label;
