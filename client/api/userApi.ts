09b75d18768b23d6b8cae3a1c4ce600a1efe0930
bg-shorthand <hanhanhan333@gmail.com>
2022-01-04 18:36:29 +0900

    feat: userApi get by email

--- client/api/userApi.ts
+++ client/api/userApi.ts
@@ -4,6 +4,9 @@ import { NewUser } from 'const/types';
 const url = process.env.NEXT_PUBLIC_DB_URL || '';
 
 const userApi = {
+  async get(email: String) {
+    return await axios.get(url + '/user/email' + email);
+  },
   async post(newUser: NewUser) {
     return await axios.post(url + '/user', newUser);
   },
