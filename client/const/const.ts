0618fadfe2c54f5ac9e27b6ed1632ae975ed5d2c
bg-shorthand <hanhanhan333@gmail.com>
2022-01-07 08:51:27 +0900

    modify: 비밀번호 정규식

--- client/const/const.ts
+++ client/const/const.ts
@@ -1,4 +1,4 @@
 const emailRegex = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
-const pwdRegex = /(?=.*\d{1,50})(?=.*[~`!@#$%\^&*()-+=]{1,50})(?=.*[a-zA-Z]{2,50}).{8,50}$/;
+const pwdRegex = /(?=.*\d{1,50})(?=.*[~`!@#$%\^&*()-+=]{1,50})(?=.*[a-zA-Z]{1,50}).{8,50}$/;
 
 export { emailRegex, pwdRegex };
