852047d588b53d2ef7644bcebc944f6fc54c1e37
bg-shorthand <hanhanhan333@gmail.com>
2022-01-07 09:05:09 +0900

    feat: 회원가입 안내 문구

--- client/components/organisms/signup/Signup.tsx
+++ client/components/organisms/signup/Signup.tsx
@@ -3,6 +3,7 @@ import Button from '@atoms/button/Button';
 import Heading from '@atoms/heading/Heading';
 import EmailAuthForm from '@molecules/emailAuthForm/EmailAuthForm';
 import SignupForm from '@molecules/signupForm/SignupForm';
+import useCloseModalDialog from 'hooks/useCloseModalDialog';
 import { useState } from 'react';
 import style from './Signup.module.scss';
 
@@ -11,14 +12,29 @@ const Signup = () => {
   const [email, setEmail] = useState('');
   const [isComplete, setIsComplete] = useState(false);
 
+  const closeDialogHandler = useCloseModalDialog();
+
   return (
     <>
       <Heading className={style.heading}>회원 가입</Heading>
       {stage === 0 && (
-        <EmailAuthForm email={email} setEmail={setEmail} setIsComplete={setIsComplete} />
+        <>
+          <Alert>인증된 이메일은 ID로 사용됩니다.</Alert>
+          <EmailAuthForm email={email} setEmail={setEmail} setIsComplete={setIsComplete} />
+        </>
       )}
-      {stage === 1 && <SignupForm email={email} setStage={setStage} />}
-      {stage === 2 && <Alert>완료</Alert>}
+      {stage === 1 && (
+        <>
+          <Alert>인증된 이메일은 ID로 사용됩니다.</Alert>
+          <SignupForm email={email} setStage={setStage} />
+        </>
+      )}
+      {stage === 2 && (
+        <>
+          <Alert>환영합니다. 로그인 해주세요.</Alert>
+          <Button onClick={closeDialogHandler}>완료</Button>
+        </>
+      )}
       {stage === 0 && (
         <Button onClick={() => setStage((pre) => (pre += 1))} disabled={!isComplete}>
           다음
