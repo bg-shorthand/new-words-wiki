c0d41df029bc4693ba5d0b3abe0fc45ed6513638
bg-shorthand <hanhanhan333@gmail.com>
2022-01-07 12:21:53 +0900

    feat: 다이얼로그 탭 이동

--- client/components/containers/dialog/Dialog.tsx
+++ client/components/containers/dialog/Dialog.tsx
@@ -1,15 +1,32 @@
 import Button from '@atoms/button/Button';
 import useControlDialog from 'hooks/useControlDialog';
-import { HTMLAttributes } from 'react';
+import { HTMLAttributes, useEffect, useRef } from 'react';
 import style from './Dialog.module.scss';
 
 const Dialog = ({ children }: HTMLAttributes<HTMLElement>) => {
   const { closeDialogAllHandler } = useControlDialog();
+  const ref = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    const $firstFocusible = ref.current?.querySelectorAll('input, button')[0] as HTMLElement;
+
+    $firstFocusible?.focus();
+  }, []);
 
   return (
-    <section className={style.container}>
+    <section className={style.container} ref={ref}>
       {children}
-      <Button onClick={closeDialogAllHandler}>X</Button>
+      <Button
+        onClick={closeDialogAllHandler}
+        onKeyDown={(e) => {
+          e.preventDefault();
+          if (e.key !== 'Tab') return;
+          const $firstFocusible = ref.current?.querySelectorAll('input, button')[0] as HTMLElement;
+          $firstFocusible.focus();
+        }}
+      >
+        X
+      </Button>
     </section>
   );
 };
