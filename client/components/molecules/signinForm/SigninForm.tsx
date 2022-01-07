6343404aa498dd7b6c4376c299003ba638e1a687
bg-shorthand <hanhanhan333@gmail.com>
2022-01-07 12:58:58 +0900

    feat: SigninForm markup

--- client/components/molecules/signinForm/SigninForm.tsx
+++ client/components/molecules/signinForm/SigninForm.tsx
@@ -1,24 +1,41 @@
 import Button from '@atoms/button/Button';
-import Input from '@atoms/input/Input';
-import Label from '@atoms/label/Label';
-import LabelInputContainer from '@containers/labelInputContainer/LabelInputContainer';
+import LabelInput from '@molecules/labelInput/LabelInput';
+import useValidString from 'hooks/useValidString';
 import { useState } from 'react';
 import style from './SigninForm.module.scss';
 
 const SigninForm = () => {
-  const [id, setId] = useState('');
-  const [pwd, setPwd] = useState('');
+  const [email, setEmail] = useState('');
+  const [passwrod, setPassword] = useState('');
+
+  const { isCorrect: isCorrectEmail, validStringHandler: validEmailHandler } =
+    useValidString('email');
+  const { isCorrect: isCorrectPassword, validStringHandler: validPasswordhandler } =
+    useValidString('password');
 
   return (
     <form className={style.container}>
-      <LabelInputContainer>
-        <Label htmlFor="signinId">ID</Label>
-        <Input id="signinId" value={id} onChange={(e) => setId(e.currentTarget.value)} />
-      </LabelInputContainer>
-      <LabelInputContainer>
-        <Label htmlFor="signinPwd">Password</Label>
-        <Input id="signinPwd" value={pwd} onChange={(e) => setPwd(e.currentTarget.value)} />
-      </LabelInputContainer>
+      <LabelInput
+        id="signinEmail"
+        label="이메일"
+        value={email}
+        onChange={(e) => setEmail(e.currentTarget.value)}
+        onBlur={validEmailHandler}
+        validations={[{ isAlert: !isCorrectEmail, alert: '이메일 형식을 확인해주세요.' }]}
+      />
+      <LabelInput
+        id="signinPassword"
+        label="비밀번호"
+        value={passwrod}
+        onChange={(e) => setPassword(e.currentTarget.value)}
+        onBlur={validPasswordhandler}
+        validations={[
+          {
+            isAlert: !isCorrectPassword,
+            alert: '비밀번호는 영문, 숫자, 특수문자를 포함해 8자리 이상이어야 합니다.',
+          },
+        ]}
+      />
       <Button>로그인</Button>
     </form>
   );
