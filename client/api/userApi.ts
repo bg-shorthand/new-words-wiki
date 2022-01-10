b8d87669d7befaceb27b436059dcb558ceb41856
bg-shorthand <hanhanhan333@gmail.com>
2022-01-10 10:28:19 +0900

    feat: isSignin api

--- client/api/userApi.ts
+++ client/api/userApi.ts
@@ -4,14 +4,21 @@ import { NewUser } from 'const/types';
 const url = process.env.NEXT_PUBLIC_DB_URL + '/user';
 
 const userApi = {
+  async signup(newUser: NewUser) {
+    return await axios.post(url, newUser);
+  },
   async signin(email: string, password: string) {
     return await axios.get(url + '/signin/?email=' + email + '&password=' + password);
   },
+  async isSignin(token: string) {
+    return await axios.get(url + '/me', {
+      headers: {
+        token: token,
+      },
+    });
+  },
   async get(email: String) {
     return await axios.get(url + '/email/' + email);
-  },
-  async signup(newUser: NewUser) {
-    return await axios.post(url, newUser);
   },
 };
 
