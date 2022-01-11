a109f4c706188e633d83aa65a22e3179f7698b74
bg-shorthand <hanhanhan333@gmail.com>
2022-01-11 09:35:38 +0900

    feat: 로그인 상태에 따라 로그인/로그아웃 버튼 노출

--- client/components/molecules/nav/Nav.tsx
+++ client/components/molecules/nav/Nav.tsx
@@ -1,20 +1,21 @@
 import Button from '@atoms/button/Button';
 import useControlDialog from 'hooks/useControlDialog';
 import useIsSignin from 'hooks/useIsSignin';
+import useSignout from 'hooks/useSignout';
 import style from './Nav.module.scss';
 
 const Nav = () => {
   const { openDialogHandler: openSigninDialog } = useControlDialog('signin');
   const { openDialogHandler: openSignupDialog } = useControlDialog('signup');
-  const { isSignin, user } = useIsSignin();
+  const { isSignin } = useIsSignin();
+  const { signout } = useSignout();
 
   return (
     <ul className={style.container}>
       {isSignin ? (
         <>
-          <li>{user?.nickname}</li>
           <li>
-            <Button>로그아웃</Button>
+            <Button onClick={() => signout()}>로그아웃</Button>
           </li>
         </>
       ) : (
