811b295165b287be3128960361a320e6e67e2185
bg-shorthand <hanhanhan333@gmail.com>
2022-01-11 18:10:15 +0900

    feat: style

--- client/components/organisms/signin/Signin.tsx
+++ client/components/organisms/signin/Signin.tsx
@@ -1,13 +1,10 @@
 import Heading from '@atoms/heading/Heading';
 import SigninForm from '@molecules/signinForm/SigninForm';
-import style from './Signin.module.scss';
 
 const Signin = () => {
   return (
     <>
-      <Heading level={1} className={style.heading}>
-        로그인
-      </Heading>
+      <Heading level={1}>로그인</Heading>
       <SigninForm />
     </>
   );
