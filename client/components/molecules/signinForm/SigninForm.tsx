d5ff07504ce98b417d1105ffe4efa5b0c967aea8
bg-shorthand <hanhanhan333@gmail.com>
2022-01-12 12:19:40 +0900

    modify: 이메일, 비밀번호 확인 로직을 signin 함수로 넘김

--- client/components/molecules/signinForm/SigninForm.tsx
+++ client/components/molecules/signinForm/SigninForm.tsx
@@ -1,9 +1,7 @@
-import Alert from '@atoms/alert/Alert';
 import Button from '@atoms/button/Button';
 import LabelInput from '@molecules/labelInput/LabelInput';
 import useControlDialog from 'hooks/useControlDialog';
 import useSignin from 'hooks/useSignin';
-import useValidString from 'hooks/useValidString';
 import { useEffect, useState } from 'react';
 import style from './SigninForm.module.scss';
 
@@ -12,16 +10,15 @@ const SigninForm = () => {
   const [password, setPassword] = useState('');
   const [canSignin, setCanSignin] = useState(false);
 
-  const { isCorrect: isCorrectEmail, validStringHandler: validEmailHandler } =
-    useValidString('email');
-  const { isCorrect: isCorrectPassword, validStringHandler: validPasswordhandler } =
-    useValidString('password');
-  const { err, signin } = useSignin(email, password);
+  const { err, isCorrectEmail, isCorrectPassword, wrongEmail, wrongPassword, signin } = useSignin(
+    email,
+    password,
+  );
   const { closeDialogHandler } = useControlDialog('signin');
 
   useEffect(() => {
-    setCanSignin(!!email && !!password && isCorrectEmail && isCorrectPassword);
-  }, [email, password, isCorrectEmail, isCorrectPassword]);
+    setCanSignin(!!email && !!password);
+  }, [email, password]);
 
   return (
     <form className={style.container}>
@@ -30,8 +27,10 @@ const SigninForm = () => {
         label="이메일"
         value={email}
         onChange={(e) => setEmail(e.currentTarget.value)}
-        onBlur={validEmailHandler}
-        validations={[{ isAlert: !isCorrectEmail, alert: '이메일 형식을 확인해주세요.' }]}
+        validations={[
+          { isAlert: !isCorrectEmail, alert: '이메일 형식을 확인해주세요.' },
+          { isAlert: wrongEmail, alert: err },
+        ]}
       />
       <LabelInput
         id="signinPassword"
@@ -39,24 +38,25 @@ const SigninForm = () => {
         type="password"
         value={password}
         onChange={(e) => setPassword(e.currentTarget.value)}
-        onBlur={validPasswordhandler}
         validations={[
           {
             isAlert: !isCorrectPassword,
             alert: '비밀번호는 영문, 숫자, 특수문자를 포함해 8자리 이상이어야 합니다.',
           },
+          { isAlert: wrongPassword, alert: err },
         ]}
       />
       <Button
-        onClick={(e) => {
-          signin();
-          closeDialogHandler && closeDialogHandler(e);
+        type="submit"
+        onClick={async (e) => {
+          e.preventDefault();
+          const success = await signin();
+          if (success && closeDialogHandler) closeDialogHandler(e);
         }}
         disabled={!canSignin}
       >
         로그인
       </Button>
-      {err && <Alert>{err}</Alert>}
     </form>
   );
 };
--- client/hooks/useSignin.ts
+++ client/hooks/useSignin.ts
@@ -2,23 +2,40 @@ import { isSigninState } from '@recoil/isSignin';
 import userApi from 'api/userApi';
 import { useState } from 'react';
 import { useSetRecoilState } from 'recoil';
+import useValidString from './useValidString';
 
 const useSignin = (email: string, password: string) => {
   const [err, setErr] = useState('');
+  const [wrongEmail, setWrongEmail] = useState(false);
+  const [wrongPassword, setWrongPassword] = useState(false);
+
   const setIsSignin = useSetRecoilState(isSigninState);
 
+  const { isCorrect: isCorrectEmail, validString: validEmail } = useValidString('email');
+  const { isCorrect: isCorrectPassword, validString: validPassword } = useValidString('password');
+
   const signin = async () => {
+    setWrongEmail(false);
+    setWrongPassword(false);
+    if (!validEmail(email) || !validPassword(password)) return false;
+
     const { data } = await userApi.signin(email, password);
-    if (data.msg) setErr(data.msg);
-    else {
+
+    if (data.msg) {
+      setErr(data.msg);
+      if (data.msg.indexOf('이메일') !== -1) setWrongEmail(true);
+      else if (data.msg.indexOf('비밀번호') !== -1) setWrongPassword(true);
+      return false;
+    } else {
       const { accessToken, refreshToken } = data;
       localStorage.setItem('access', accessToken);
       localStorage.setItem('refresh', refreshToken);
       setIsSignin(true);
+      return true;
     }
   };
 
-  return { err, signin };
+  return { err, isCorrectEmail, isCorrectPassword, wrongEmail, wrongPassword, signin };
 };
 
 export default useSignin;
