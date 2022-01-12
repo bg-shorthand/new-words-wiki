cedb5491ef3a8f2802a1364334aad374e92fd63f
bg-shorthand <hanhanhan333@gmail.com>
2022-01-12 14:04:29 +0900

    modify: 핸들러를 함수로 바꾸고 결과 불리언 리턴

--- client/hooks/useSignup.ts
+++ client/hooks/useSignup.ts
@@ -1,26 +1,24 @@
 import userApi from 'api/userApi';
 import { NewUser } from 'const/types';
-import { MouseEventHandler, useState } from 'react';
+import { useState } from 'react';
 
 const useSignup = (newUser: NewUser) => {
   const [isUniqueEmail, setIsUniqueEmail] = useState(true);
   const [isUniqueNickname, setIsUniqueNickname] = useState(true);
-  const [isSuccess, setIsSuccess] = useState(false);
 
-  const signupHandler: MouseEventHandler = async (e) => {
-    e.preventDefault();
+  const signup = async () => {
     const { data } = await userApi.signup(newUser);
     if (data.key === 'email') {
       setIsUniqueEmail(false);
-      setTimeout(() => setIsUniqueEmail(true), 3000);
+      return false;
     } else if (data.key === 'nickname') {
       setIsUniqueNickname(false);
-      setTimeout(() => setIsUniqueNickname(true), 3000);
+      return false;
     } else if (data.success) {
-      setIsSuccess(true);
-    }
+      return true;
+    } else return false;
   };
-  return { isUniqueEmail, isUniqueNickname, isSuccess, signupHandler };
+  return { isUniqueEmail, isUniqueNickname, signup };
 };
 
 export default useSignup;
