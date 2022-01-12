b6aaff6d4bc2903928c515ac4a52b79a263e8ced
bg-shorthand <hanhanhan333@gmail.com>
2022-01-12 13:06:03 +0900

    feat: 이메일 형식 확인, 결과 리턴

--- client/hooks/useSetEmailAuthKey.ts
+++ client/hooks/useSetEmailAuthKey.ts
@@ -1,23 +1,33 @@
 import emailAuthApi from 'api/emailAuth';
 import userApi from 'api/userApi';
 import { useState } from 'react';
+import useValidString from './useValidString';
 
 const useSetEmailAuthKey = () => {
   const [liveTime, setLiveTime] = useState(0);
   const [isUniqueEmail, setIsUniqueEmail] = useState(true);
 
+  const { isCorrect, validString } = useValidString('email');
+
   const setEmailAuthKey = async (email: string) => {
+    setLiveTime(0);
+    setIsUniqueEmail(true);
+
+    if (!validString(email)) return false;
+
     const { data } = await userApi.get(email);
 
     if (data.isUser) {
       setIsUniqueEmail(false);
+      return false;
     } else {
       const { data } = await emailAuthApi.post(email);
       setLiveTime(data.liveTime);
+      return true;
     }
   };
 
-  return { isUniqueEmail, liveTime, setEmailAuthKey };
+  return { isUniqueEmail, isCorrect, liveTime, setEmailAuthKey };
 };
 
 export default useSetEmailAuthKey;
