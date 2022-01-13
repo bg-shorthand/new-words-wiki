5076121598fffa22bb6ea42dcb30fbedc61f5eec
bg-shorthand <hanhanhan333@gmail.com>
2022-01-13 09:36:19 +0900

    feat: signin footer layout

--- client/components/organisms/signin/Signin.tsx
+++ client/components/organisms/signin/Signin.tsx
@@ -1,11 +1,16 @@
 import Heading from '@atoms/heading/Heading';
+import LabelInput from '@molecules/labelInput/LabelInput';
 import SigninForm from '@molecules/signinForm/SigninForm';
+import style from './Signin.module.scss';
 
 const Signin = () => {
   return (
     <>
       <Heading level={1}>로그인</Heading>
       <SigninForm />
+      <div className={style.footer}>
+        <LabelInput id="keepSignin" label="로그인 유지" type="checkbox" />
+      </div>
     </>
   );
 };
