2ed0c86e5080c25f6b179b887b212cbce5f18996
bg-shorthand <hanhanhan333@gmail.com>
2022-01-13 13:45:19 +0900

    feat: signin footer markup, layout

--- client/components/organisms/signin/Signin.tsx
+++ client/components/organisms/signin/Signin.tsx
@@ -1,14 +1,26 @@
+import Button from '@atoms/button/Button';
 import Heading from '@atoms/heading/Heading';
-import LabelInput from '@molecules/labelInput/LabelInput';
 import SigninForm from '@molecules/signinForm/SigninForm';
+import { dialogsState } from '@recoil/modalDialog';
+import { useSetRecoilState } from 'recoil';
 import style from './Signin.module.scss';
 
 const Signin = () => {
+  const setDialogs = useSetRecoilState(dialogsState);
+
   return (
     <>
       <Heading level={1}>로그인</Heading>
       <SigninForm />
-      <div className={style.footer}></div>
+      <div className={style.footer}>
+        <Button
+          size="s"
+          onClick={() => setDialogs((pre) => ({ ...pre, signin: false, signup: true }))}
+        >
+          회원가입
+        </Button>
+        <Button size="content">비밀번호 찾기</Button>
+      </div>
     </>
   );
 };
