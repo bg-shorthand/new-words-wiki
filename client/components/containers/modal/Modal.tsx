be9cc9b01a00d1c77153c601a1ae3cfbd3d67bd3
bg-shorthand <hanhanhan333@gmail.com>
2022-01-16 15:58:08 +0900

    feat: 모달 팝업시 스크롤 막음

--- client/components/containers/modal/Modal.tsx
+++ client/components/containers/modal/Modal.tsx
@@ -1,7 +1,15 @@
-import { HTMLAttributes } from 'react';
+import { HTMLAttributes, useEffect } from 'react';
 import style from './Modal.module.scss';
 
 const Modal = ({ children }: HTMLAttributes<HTMLDivElement>) => {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  });
+
   return <div className={style.container}>{children}</div>;
 };
 
