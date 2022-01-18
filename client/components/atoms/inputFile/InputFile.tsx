efa5a6e323cfffa90d7d83c472c08fe9a011ce03
bg-shorthand <hanhanhan333@gmail.com>
2022-01-18 09:28:13 +0900

    feat: InputFile

---
+++ client/components/atoms/inputFile/InputFile.tsx
@@ -0,0 +1,16 @@
+import Label from '@atoms/label/Label';
+import { InputHTMLAttributes } from 'react';
+import style from './InputFile.module.scss';
+
+const InputFile = ({ id, accept = 'image/*', ...props }: InputHTMLAttributes<HTMLInputElement>) => {
+  return (
+    <>
+      <Label htmlFor={id} className={style.label}>
+        <i className="fas fa-upload"></i> 이미지 업로드
+      </Label>
+      <input type="file" className={style.input} id={id} accept={accept} {...props} />
+    </>
+  );
+};
+
+export default InputFile;
