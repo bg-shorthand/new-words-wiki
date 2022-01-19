9da697ea7d0c35b472ee2d6e69f923f044f6691a
bg-shorthand <hanhanhan333@gmail.com>
2022-01-19 14:08:56 +0900

    feat: Heading style

--- client/components/atoms/heading/Heading.tsx
+++ client/components/atoms/heading/Heading.tsx
@@ -1,4 +1,5 @@
 import { HTMLAttributes } from 'react';
+import style from './Heading.module.scss';
 
 interface HeadingProps extends HTMLAttributes<HTMLHeadingElement> {
   level: 1 | 2 | 3 | 4 | 5 | 6;
@@ -7,17 +8,17 @@ interface HeadingProps extends HTMLAttributes<HTMLHeadingElement> {
 const Heading = ({ level = 1, ...props }: HeadingProps) => {
   switch (level) {
     case 1:
-      return <h1 {...props} />;
+      return <h1 className={style.h1} {...props} />;
     case 2:
-      return <h2 {...props} />;
+      return <h2 className={style.h2} {...props} />;
     case 3:
-      return <h3 {...props} />;
+      return <h3 className={style.h3} {...props} />;
     case 4:
-      return <h4 {...props} />;
+      return <h4 className={style.h4} {...props} />;
     case 5:
-      return <h5 {...props} />;
+      return <h5 className={style.h5} {...props} />;
     case 6:
-      return <h6 {...props} />;
+      return <h6 className={style.h6} {...props} />;
     default:
       return null;
   }
