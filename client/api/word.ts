f62c143c9972cb315285be10ab8c212e465a3920
bg-shorthand <hanhanhan333@gmail.com>
2022-01-19 13:29:52 +0900

    feat: encodeURI

--- client/api/word.ts
+++ client/api/word.ts
@@ -4,6 +4,9 @@ import { Word } from 'const/types';
 const url = process.env.NEXT_PUBLIC_DB_URL + '/word';
 
 const wordApi = {
+  async get(title: string) {
+    return await axios.get(encodeURI(url + '?title=' + title));
+  },
   async post(newWord: Word, access: string, refresh: string) {
     return await axios.post(url, newWord, {
       headers: {
