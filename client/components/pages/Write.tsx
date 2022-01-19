722bb7ce3d1e8d76e0b163d91decd165105f3dc8
bg-shorthand <hanhanhan333@gmail.com>
2022-01-19 12:13:36 +0900

    feat: 로그인 확인해서 등록

--- client/components/pages/Write.tsx
+++ client/components/pages/Write.tsx
@@ -1,10 +1,12 @@
 import Button from '@atoms/button/Button';
 import Content from '@containers/content/Content';
+import useOpenAlertDialog from '@hooks/useOpenAlertDialog';
 import ImageUploader from '@molecules/imageUploader/ImageUploader';
 import LabelInput from '@molecules/labelInput/LabelInput';
 import LabelTextArea from '@molecules/labelTextArea/LabelTextArea';
 import MainLayout from '@templates/mainLayout/MainLayout';
 import { wordApi } from 'api/word';
+import setToken from 'modules/setToken';
 import { useState } from 'react';
 
 const Write = () => {
@@ -12,6 +14,8 @@ const Write = () => {
   const [definition, setDefinition] = useState('');
   const [history, setHistory] = useState('');
   const [images, setImages] = useState<string[]>([]);
+
+  const openAlertDialog = useOpenAlertDialog();
 
   return (
     <MainLayout>
@@ -52,7 +56,15 @@ const Write = () => {
       <Content fitContent>
         <Button
           onClick={async (e) => {
-            const res = await wordApi.post({ title, definition, history, images });
+            const payload = { title, definition, history, images };
+            const { access, refresh } = setToken.get();
+            if (!access || !refresh) return openAlertDialog('로그인이 필요합니다.');
+            const { data } = await wordApi.post(payload, access, refresh);
+            if (!data.success) {
+              if (data.errMsg) return openAlertDialog(data.errMsg);
+              else if (data.newAccess) await wordApi.post(payload, data.newAccess, refresh);
+              else console.log(data.errMsg);
+            } else openAlertDialog('등록되었습니다.');
           }}
           disabled={!title.length && !definition.length}
         >
