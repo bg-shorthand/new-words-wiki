0ee327f9d3d5b58f1d3aa1ebdd4bf65828b11148
bg-shorthand <hanhanhan333@gmail.com>
2022-01-19 11:00:36 +0900

    feat: 로그아웃 상태에서는 얼럿 팝업

--- client/components/molecules/nav/Nav.tsx
+++ client/components/molecules/nav/Nav.tsx
@@ -6,17 +6,31 @@ import useSignout from '@hooks/useSignout';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import style from './Nav.module.scss';
 import Link from 'next/link';
+import useOpenAlertDialog from '@hooks/useOpenAlertDialog';
 
 const Nav = () => {
   const isSignin = useRecoilValue(isSigninState);
   const myInfo = useRecoilValue(myInfoState);
   const setDialogs = useSetRecoilState(dialogsState);
+
+  const openAlertDialog = useOpenAlertDialog();
   const { signout } = useSignout();
 
   return (
     <ul className={style.container}>
       <li>
-        <Link href="/write">신조어 등록</Link>
+        <Link href="/write">
+          <a
+            onClick={(e) => {
+              if (!isSignin) {
+                e.preventDefault();
+                openAlertDialog('로그인이 필요합니다.');
+              }
+            }}
+          >
+            신조어 등록
+          </a>
+        </Link>
       </li>
       {isSignin ? (
         <>
