10a9a70ae1a0750eccb98bd7e5eea1d907f53148
bg-shorthand <hanhanhan333@gmail.com>
2022-01-20 11:11:39 +0900

    feat: auto complete

--- client/components/molecules/search/Search.tsx
+++ client/components/molecules/search/Search.tsx
@@ -1,22 +1,41 @@
 import IconButton from '@atoms/iconButton/IconButton';
 import LabelInput from '@molecules/labelInput/LabelInput';
+import { wordApi } from 'api/word';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import style from './Search.module.scss';
 
 const Search = () => {
   const [word, setWord] = useState('');
+  const [titles, setTitles] = useState([]);
 
   const router = useRouter();
+
+  useEffect(() => {
+    const setTitlesAsync = async () => {
+      const { data } = await wordApi.getAll();
+      setTitles(data.data);
+    };
+    setTitlesAsync();
+  }, []);
 
   return (
     <form className={style.container}>
       <LabelInput
         id="keyword"
         label="검색어"
+        list="searchWord"
         value={word}
         onChange={(e) => setWord(e.currentTarget.value)}
       />
+      {word.length ? (
+        <datalist id="searchWord">
+          {titles &&
+            titles.map((title: string) => {
+              return <option value={title}></option>;
+            })}
+        </datalist>
+      ) : null}
       <IconButton
         aria-hidden
         type="submit"
