3feb50049d6db3b7b945e5d7c1d10c231c867e8f
bg-shorthand <hanhanhan333@gmail.com>
2022-01-20 12:37:44 +0900

    feat: style

--- client/components/organisms/header/Header.tsx
+++ client/components/organisms/header/Header.tsx
@@ -1,15 +1,19 @@
 import Heading from '@atoms/heading/Heading';
 import Nav from '@molecules/nav/Nav';
 import Search from '@molecules/search/Search';
+import Image from 'next/image';
 import Link from 'next/link';
 import style from './Header.module.scss';
+import logo from '../../../public/logo.png';
 
 const Header = () => {
   return (
     <header className={style.container}>
       <Heading level={1}>
         <Link href="/">
-          <a>New Words Wiki</a>
+          <a>
+            <Image src={logo} width={20} height={20} /> 신조어 위키
+          </a>
         </Link>
       </Heading>
       <Search />
