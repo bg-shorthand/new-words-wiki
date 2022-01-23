acfc0bcc157ee251e4170ad459357eb68e3b41c8
bg-shorthand <hanhanhan333@gmail.com>
2022-01-23 12:28:54 +0900

    feat: verifyToken

--- server/routes/report.js
+++ server/routes/report.js
@@ -12,7 +12,7 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
+router.post('/', verifyToken, async (req, res) => {
   try {
     const report = req.body;
     const newReport = await Report.create(report);
@@ -23,7 +23,7 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.delete('/', async (req, res) => {
+router.delete('/', verifyToken, async (req, res) => {
   try {
     const { id } = req.query;
     const data = await Report.deleteById(id);
