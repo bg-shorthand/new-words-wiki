041c9c9ab6acb300b9ce9f4abf4dca0b1dc95a2b
bg-shorthand <hanhanhan333@gmail.com>
2022-01-28 19:52:41 +0900

    feat: 게시글 없을 때 처리

--- client/pages/community/[page].tsx
+++ client/pages/community/[page].tsx
@@ -1,33 +1,45 @@
+import Button from '@atoms/button/Button';
+import Heading from '@atoms/heading/Heading';
 import Community from '@components/pages/Community';
+import Content from '@containers/content/Content';
+import MainLayout from '@templates/mainLayout/MainLayout';
 import communityApi from 'api/community';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { useRouter } from 'next/router';
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { page } = context.params as { page: string };
   const { data } = await communityApi.get(+page);
   const { posts, allLength } = data.data;
 
-  if (posts.length) {
-    return {
-      props: {
-        posts,
-        allLength,
-      },
-    };
-  } else
-    return {
-      redirect: {
-        destination: '/community/1',
-        permanent: false,
-      },
-    };
+  return {
+    props: {
+      posts,
+      allLength,
+    },
+  };
 };
 
 const CommunityPage = ({
   posts,
   allLength,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  return <Community posts={posts} allLength={allLength} />;
+  const router = useRouter();
+
+  return posts.length ? (
+    <Community posts={posts} allLength={allLength} />
+  ) : (
+    <MainLayout>
+      <Content fitContent alignSelf="center">
+        <Heading level={1}>게시물이 없습니다.</Heading>
+      </Content>
+      <Content fitContent alignSelf="center">
+        <Button size="s" onClick={() => router.push('/community/writePost')}>
+          쓰기
+        </Button>
+      </Content>
+    </MainLayout>
+  );
 };
 
 export default CommunityPage;
