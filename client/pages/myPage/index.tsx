b2f74f89d4b60f2544e2c246f071ef6ed9a0e76a
bg-shorthand <hanhanhan333@gmail.com>
2022-01-31 10:35:31 +0900

    feat: 비로그인시 리다이렉트

--- client/pages/myPage/index.tsx
+++ client/pages/myPage/index.tsx
@@ -1,6 +1,18 @@
 import MyPage from '@components/pages/MyPage';
+import { isSigninState } from '@recoil/isSignin';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
 
 const MyPagePage = () => {
+  const isSignin = useRecoilValue(isSigninState);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isSignin) router.replace('/');
+  }, [isSignin]);
+
   return <MyPage />;
 };
 
