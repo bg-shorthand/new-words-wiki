0ce264aab83eba59add9d83adfbff1ceda7bf880
bg-shorthand <hanhanhan333@gmail.com>
2022-02-09 10:37:30 +0900

    feat: dev environment

---
+++ server/dev_app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const whitelist = [process.env.LOCALHOST];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not Allowed Origin!'));
+    }
+  },
+};
+
+const PORT = process.env.PORT;
+const DEV_MONGO_URI = process.env.DEV_MONGO_URI;
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
+// app.use(cors(corsOptions));
+app.use(cors());
+
+mongoose
+  .connect(DEV_MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Successfully connected to mongodb'))
+  .catch((e) => console.error(e));
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
+app.use('/user', require('./routes/user'));
+app.use('/emailAuth', require('./routes/emailAuth'));
+app.use('/word', require('./routes/word'));
+app.use('/report', require('./routes/report'));
+app.use('/community', require('./routes/community'));
