36527600384848d9140ef6d90dd0908ae9b218d2
bg-shorthand <hanhanhan333@gmail.com>
2022-02-16 21:30:13 +0900

    client, server dir

--- client/before.sh
+++ client/before.sh
@@ -3,6 +3,3 @@
 if [ -d /home/ubuntu/client ]; then
     rm -rf /home/ubuntu/client
 fi
-if [ -d /home/ubuntu/server ]; then
-    rm -rf /home/ubuntu/server
-fi
--- server/before.sh
+++ server/before.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-if [ -d /home/ubuntu/client ]; then
-    rm -rf /home/ubuntu/client
-fi
 if [ -d /home/ubuntu/server ]; then
     rm -rf /home/ubuntu/server
 fi
