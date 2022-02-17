e754a58b3dbca0cd03c9bee0a55f18debc5f5526
bg-shorthand <hanhanhan333@gmail.com>
2022-02-17 11:31:43 +0900

    src

--- src/before.sh
+++ src/before.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-if [ -d /home/ubuntu/client ]; then
-    rm -rf /home/ubuntu/client
-fi
-if [ -d /home/ubuntu/server ]; then
-    rm -rf /home/ubuntu/server
+if [ -d /home/ubuntu/src ]; then
+    rm -rf /home/ubuntu/src
 fi
